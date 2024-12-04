aa5d1ba877c16fb549f9e46bc18d45806c11ba22
reesaldle <156384573+reesaldles@users.noreply.github.com>
2024-12-03 18:38:58 -0600

    more stuff :)

---
+++ public/index.js
@@ -0,0 +1,7 @@
+const routes = [
+    { path: "/home", file: "home.html" },
+    { path: "/contact", file: "contact.html" },
+    { path: "/about", file: "about.html" },
+    { path: "/blog", file: "blog.html" },
+    { path: "/error", file: "/assets/error/404.html" },
+];
