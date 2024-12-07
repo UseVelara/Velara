5865df99a914dd85a0ab188435898841b4c1319f
reesaldle <156384573+reesaldles@users.noreply.github.com>
2024-12-07 15:17:38 -0600

    uv static better now i think idfk

---
+++ public/static/uv/uv.config.js
@@ -0,0 +1,12 @@
+self.__uv$config = {
+    prefix: '/static/uv/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/static/uv/uv.config.js',
+    sw: '/static/uv/uv.sw.js',
+};
+
+// https://freediddyandrkelly.home.kg/bare/ brunys was here
